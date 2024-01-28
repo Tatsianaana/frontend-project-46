676606b73c33c84295fccf88192ed69f1898d069
Tatsianaana <kazachaya.tatyana@mail.ru>
2024-01-28 17:23:27 +0300

    update src/parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,16 +1,16 @@
 import yaml from 'js-yaml';
 
 const parse = (data, dataFormat) => {
-    switch (dataFormat) {
-        case 'json':
-            return JSON.parse(data);
-        case 'yaml':
-            return yaml.load(data);
-        case 'yml':
-            return yaml.load(data);
-        default:
-            throw new Error(`Unexpected data format ${dataFormat}`);
-    }
+  switch (dataFormat) {
+    case 'json':
+      return JSON.parse(data);
+    case 'yaml':
+      return yaml.load(data);
+    case 'yml':
+      return yaml.load(data);
+    default:
+      throw new Error(`Unexpected data format ${dataFormat}`);
+  }
 };
 
 export default parse;
