2b5141f39ca293b9279031dcc6bac7221cddaff9
Tatsianaana <kazachaya.tatyana@mail.ru>
2024-01-28 17:20:39 +0300

    update src/formatters/plain.js

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -5,35 +5,35 @@ const separator = '\n';
 const buildPath = (path, key) => (path.length > 0 ? `${path}.${key}` : String(key));
 
 const stringify = (value) => {
-    if (_.isObject(value)) {
-        return '[complex value]';
-    }
-    return (_.isString(value)) ? `'${value}'` : String(value);
+  if (_.isObject(value)) {
+    return '[complex value]';
+  }
+  return (_.isString(value)) ? `'${value}'` : String(value);
 };
 
 const formatPlain = (diff) => {
-    const iter = (tree, path) => {
-        const result = tree
-            .filter((node) => node.type !== 'unchanged')
-            .map((node) => {
-                const currentPath = buildPath(path, node.key);
-                if (node.type === 'deleted') {
-                    return `Property '${currentPath}' was removed`;
-                }
-                if (node.type === 'added') {
-                    return `Property '${currentPath}' was added with value: ${stringify(node.value)}`;
-                }
-                if (node.type === 'nested') {
-                    return iter(node.children, currentPath);
-                }
-                if (node.type === 'changed') {
-                    return `Property '${currentPath}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
-                }
-                throw new Error(`Unknown node type: '${node.type}'`);
-            });
-            return result.join(separator);
-    };
-    return iter(diff, '');
+  const iter = (tree, path) => {
+    const result = tree
+      .filter((node) => node.type !== 'unchanged')
+      .map((node) => {
+        const currentPath = buildPath(path, node.key);
+        if (node.type === 'deleted') {
+          return `Property '${currentPath}' was removed`;
+        }
+        if (node.type === 'added') {
+          return `Property '${currentPath}' was added with value: ${stringify(node.value)}`;
+        }
+        if (node.type === 'nested') {
+          return iter(node.children, currentPath);
+        }
+        if (node.type === 'changed') {
+          return `Property '${currentPath}' was updated. From ${stringify(node.value1)} to ${stringify(node.value2)}`;
+        }
+        throw new Error(`Unknown node type: '${node.type}'`);
+      });
+    return result.join(separator);
+  };
+  return iter(diff, '');
 };
 
 export default formatPlain;
