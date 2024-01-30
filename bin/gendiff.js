7e836a88a24a3643a8d5ff120f816746a0cb3e86
Tatsianaana <kazachaya.tatyana@mail.ru>
2024-01-30 18:42:55 +0300

    update ../bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,7 +9,7 @@ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .argument('<filepath1>')
   .argument('<filepath2>')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format <type>', 'output format')
   .helpOption('-h, --help', 'output usage information')
   .action((filepath1, filepath2) => {
     const options = program.opts();
