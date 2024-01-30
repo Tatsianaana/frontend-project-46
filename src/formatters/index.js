85e8e26d91d0fa669eca0ab0652caf0cd9e97e94
Tatsianaana <kazachaya.tatyana@mail.ru>
2024-01-30 11:35:58 +0300

    update src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,8 +2,8 @@ import formatStylish from './stylish.js';
 import formatPlain from './plain.js';
 import formatJSON from './json.js';
 
-const format = (diff, formatter) => {
-  switch (formatter) {
+const format = (diff, formatName) => {
+  switch (formatName) {
     case 'plain':
       return formatPlain(diff);
     case 'json':
@@ -11,7 +11,7 @@ const format = (diff, formatter) => {
     case 'stylish':
       return formatStylish(diff);
     default:
-      throw new Error(`Error: unknown formatter ${formatter}`);
+      throw new Error(`No such formatter: '${formatName}'`);
   }
 };
 
