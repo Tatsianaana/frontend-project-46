dbab935a331070e15c1bd334de585508c5874969
Tatsianaana <kazachaya.tatyana@mail.ru>
2024-02-04 17:06:03 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import _ from 'lodash';
 import parse from './parsers.js';
 import makeDiff from './makeDiff.js';
 import format from './formatters/index.js';
 
-const getAbsolutePath = (filePath) => path.resolve(process.cwd(), '../__fixtures__', filePath);
+const getAbsolutePath = (filePath) => path.resolve(process.cwd(), filePath);
 
 const excludeFormat = (filePath) => {
-  const extension = path.extname(getAbsolutePath(filePath));
-  return _.trimStart(extension, '.');
+  const extension = path.extname(getAbsolutePath(filePath)).slice(1);
+  return extension;
 };
 
 const getData = (filePath) => {
