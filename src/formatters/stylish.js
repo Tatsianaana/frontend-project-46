1b39e33b933b097334ce98b4f031609ab90eb32e
Tatsianaana <kazachaya.tatyana@mail.ru>
2024-02-13 15:30:32 +0300

    update src/formatters/stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -21,24 +21,20 @@ const stringify = (data, depth) => {
 const formatStylish = (diff) => {
   const iter = (tree, depth) => {
     const result = tree.map((node) => {
-      if (node.type === 'deleted') {
-        return `${setIndentation(depth + 1, spaceCount, offsetCount)}- ${node.key}: ${stringify(node.value, depth + 1)}`;
+      switch (node.type) {
+        case 'deleted':
+          return `${setIndentation(depth + 1, spaceCount, offsetCount)}- ${node.key}: ${stringify(node.value, depth + 1)}`;
+        case 'added':
+          return `${setIndentation(depth + 1, spaceCount, offsetCount)}+ ${node.key}: ${stringify(node.value, depth + 1)}`;
+        case 'nested':
+          return `${setIndentation(depth + 1, spaceCount)}${node.key}: ${iter(node.children, depth + 1)}`;
+        case 'changed':
+          return `${setIndentation(depth + 1, spaceCount, offsetCount)}- ${node.key}: ${stringify(node.value1, depth + 1)}\n${setIndentation(depth + 1, spaceCount, offsetCount)}+ ${node.key}: ${stringify(node.value2, depth + 1)}`;
+        case 'unchanged':
+          return `${setIndentation(depth + 1, spaceCount)}${node.key}: ${stringify(node.value, depth)}`;
+        default:
+          throw new Error(`Unknown node type: '${node.type}'`);
       }
-      if (node.type === 'added') {
-        return `${setIndentation(depth + 1, spaceCount, offsetCount)}+ ${node.key}: ${stringify(node.value, depth + 1)}`;
-      }
-      if (node.type === 'nested') {
-        return `${setIndentation(depth + 1, spaceCount)}${node.key}: ${iter(node.children, depth + 1)}`;
-      }
-      if (node.type === 'changed') {
-        const line1 = `${setIndentation(depth + 1, spaceCount, offsetCount)}- ${node.key}: ${stringify(node.value1, depth + 1)}`;
-        const line2 = `${setIndentation(depth + 1, spaceCount, offsetCount)}+ ${node.key}: ${stringify(node.value2, depth + 1)}`;
-        return `${line1}\n${line2}`;
-      }
-      if (node.type === 'unchanged') {
-        return `${setIndentation(depth + 1, spaceCount)}${node.key}: ${stringify(node.value, depth)}`;
-      }
-      throw new Error(`Unknown node type: '${node.type}'`);
     });
     return `{\n${result.join(separator)}\n${setIndentation(depth, spaceCount)}}`;
   };
